2bc64fa0c69c76965caa0e2a92155a7619193bc9
Lukasz Wojdat <lukaszwojdat88@gmail.com>
2020-08-23 18:17:24 +0200

    Add improvement to adding a task directly from calendar

--- src/components/pages/TaskAdd/TaskAdd.js
+++ src/components/pages/TaskAdd/TaskAdd.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import TaskForm from 'components/common/TaskForm';
@@ -6,10 +8,18 @@ import API from 'store/api';
 import SELECTORS from 'store/selectors';
 
 const TaskAdd = ({ token }) => {
+
+  const location = useLocation();
+  const { month, year, day } = queryString.parse(location.search);
+
   const initialValues = {
     title: '',
     priority: 1,
     description: '',
+    endDate: (month && year && day) ? 
+      `${year}-${month > 9 ? month : `0${month}`}-${day > 9 ? day: `0${day}`}` 
+      : 
+      '',
   };
 
   return ( 
