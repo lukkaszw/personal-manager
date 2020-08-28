5875e48b944400a07371036edc81d59ce80a25c4
Lukasz Wojdat <lukaszwojdat88@gmail.com>
2020-08-28 18:11:20 +0200

    Improve BudgetList styles

--- src/components/pages/Budget/components/BudgetListData/BudgetListData.styles.js
+++ src/components/pages/Budget/components/BudgetListData/BudgetListData.styles.js
@@ -8,9 +8,20 @@ export const Root = styled.div`
   justify-content: space-between;
 `;
 
-export const useStyles = makeStyles({
+export const useStyles = makeStyles((theme) => ({
   listItem: {
     padding: '0 10px',
     borderBottom: '1px solid #555',
+    transition: '.3s',
+
+    '&:hover': {
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.font.secondary.main,
+      borderRadius: '0 0 50px 5px',
+
+      '& p': {
+        color: theme.palette.font.secondary.darker,
+      },
+    }
   },
-});
+}));
