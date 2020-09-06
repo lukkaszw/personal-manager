3356a006c460e738987d7e2d37a4e29fc8bca1e0
Lukasz Wojdat <lukaszwojdat88@gmail.com>
2020-09-06 18:53:27 +0200

    correct baseUrl

--- src/utils/api.endpoints.js
+++ src/utils/api.endpoints.js
@@ -1,5 +1,5 @@
 const api = {
-  baseUrl: process.env.NODE_ENV === 'production' ? 'api' : 'http://localhost:8000/api',
+  baseUrl: process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:8000/api',
   endpoints: {
     user: {
       register: 'user',
